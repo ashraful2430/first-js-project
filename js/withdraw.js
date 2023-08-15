a09fe1919415f8cb3c7d3b0377af658d3fd85659
ashraful2430 <www.savage582@gmail.com>
2023-08-15 15:11:57 +0600

    final push

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,12 +1,23 @@
-document.getElementById('btn-withdraw').addEventListener('click', function(){
+document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const withdrawFieldString = withdrawField.value;
-    const newWithdraw = parseFloat(withdrawFieldString); 
-    
+    const newWithdraw = parseFloat(withdrawFieldString);
+
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const withdrawTotalElementString = withdrawTotalElement.innerText;
     const newWithdrawTotal = parseFloat(withdrawTotalElementString);
 
-    const totalWithdraw = newWithdraw+newWithdrawTotal;
+    const totalWithdraw = newWithdraw + newWithdrawTotal;
     withdrawTotalElement.innerText = totalWithdraw;
+
+    const balanceTotalElement=document.getElementById('balance-total');
+    const balanceTotalElementString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalElementString);
+
+    const finalWithdraw = previousBalanceTotal-newWithdraw;
+    balanceTotalElement.innerText= finalWithdraw;
+
+
+
+    withdrawField.value = '';
 })
