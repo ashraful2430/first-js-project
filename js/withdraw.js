40633d8a30c3cb09a927011ea4dbe285d35802c7
ashraful2430 <www.savage582@gmail.com>
2023-08-15 16:01:43 +0600

    bal

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,21 +3,37 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawFieldString = withdrawField.value;
     const newWithdraw = parseFloat(withdrawFieldString);
 
+    withdrawField.value = '';
+
+    if (isNaN(newWithdraw)){
+        alert('ata bank re bokachoda number de word na ')
+        return;
+    }
+
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const withdrawTotalElementString = withdrawTotalElement.innerText;
     const newWithdrawTotal = parseFloat(withdrawTotalElementString);
 
-    const totalWithdraw = newWithdraw + newWithdrawTotal;
-    withdrawTotalElement.innerText = totalWithdraw;
+    
 
-    const balanceTotalElement=document.getElementById('balance-total');
+
+    const balanceTotalElement = document.getElementById('balance-total');
     const balanceTotalElementString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalElementString);
 
-    const finalWithdraw = previousBalanceTotal-newWithdraw;
-    balanceTotalElement.innerText= finalWithdraw;
+    if (newWithdraw > previousBalanceTotal) {
+        alert('tor bape rakhse ato taka bank a? ')
+        return;
+    }
+
+    const totalWithdraw = newWithdraw + newWithdrawTotal;
+    withdrawTotalElement.innerText = totalWithdraw;
+
+
+    const finalWithdraw = previousBalanceTotal - newWithdraw;
+    balanceTotalElement.innerText = finalWithdraw;
 
 
 
-    withdrawField.value = '';
+
 })
